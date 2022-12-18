1f845abcb57bf68497f4536b7cc8eb10c843d217
Neosapien <anas@lecaillon.com>
2022-12-18 12:38:30 +0000

    added djikstra algorithm to graph code

--- algorithms/algorithm_graph_0/algo.py
+++ algorithms/algorithm_graph_0/algo.py
@@ -1,22 +1,30 @@
-# from matlpotlib import pyplot as plt
+# for graph operations
 import numpy as np
 import osmnx as ox
+from matplotlib import pyplot as plt
+
+# for djikstra algorithm
 from heapq import *
 from math import inf
-import matplotlib.pyplot as plt
-
-# G = ox.graph_from_place("Saint Germain en Laye, France")
-H = ox.graph_from_address("1 Venelle Artémis, Saint Germain en Laye, france")
-
-# Soit Adj la matrice d'adjacence
-
-n = 0  # taille du graph
-Adj = np.zeros((n, n))  # Avec A[i][j] la distance entre i et j, qui peut etre
-# None si i et j ne sont pas reliés
-
 
 class Graph:
-    def __init__(self, G):
+    """
+    Un Graphe est un objet enregistrant a la fois le graphe osmnx
+    ainsi qu'une forme plus exploitable, notamment avec des neouds indices de 0
+    a n et muni d'une matrice d'adjacence, et de nombreuses methodes pour faciliter
+    son exploitation
+    """
+    def __init__(self, G=None, place=None, address=None, filepath=None):
+        assert(G != None or address != None or place != None or filepath != None)
+        if G == None and address != None and filepath != None and place == None:
+            G = ox.graph_from_address(address)
+            ox.save_graphml(G, filepath)
+        elif G == None and address == None and filepath != None and place != None:
+            G = ox.graph_from_place(place)
+            ox.save_graphml(G, filepath)
+        elif G == None and address == None and place == None and filepath != None:
+            G = ox.load_graphml(filepath)
+        
         members = [i for i in G.nodes]
         n = G.order()
         adj = np.zeros((n, n))
@@ -33,8 +41,12 @@ class Graph:
         self.rep = ox.plot_graph(G)
         self.size = len(members)
         self.graph = G
+        self.dists_from = {}
 
     def deg(self, i: int) -> int:
+        """
+        renvoie le degre d'un point
+        """
         d = 0
         for j in range(self.size):
             if self.adj[i, j] != 0:
@@ -42,6 +54,9 @@ class Graph:
         return d
 
     def links(self, pt: int, n: int) -> list:
+        """
+        renvoie les noeuds a n degres de parente de pt
+        """
         if n == 0:
             return []
         S = []
@@ -55,6 +70,9 @@ class Graph:
         return S
 
     def deginf(self, pt: int, lev: int) -> int:
+        """
+        donne le degré infini (évalué a n niveaux de parenté) de chaque point
+        """
         degG = self.deg(pt)
         if lev == 0:
             return degG
@@ -64,18 +82,32 @@ class Graph:
         return deginfG
 
     def show_node(self, i: int) -> plt.plot:
+        """
+        affiche le noeud sur l'image du graphe
+        """
         return ox.plot_graph_route(self.graph, [self.members[i]])
 
     def show_nodes(self, L: list) -> plt.plot:
+        """
+        affiche les noeuds sur l'image du graphe
+        """
         lst = [self.members[i] for i in L]
         return ox.plot_graph_route(self.graph, lst)
 
     def show_neighbors(self, i: int, n: int) -> plt.plot:
+        """
+        montre les voisins sur l'image, a n degres de parente
+        """
         L = self.links(i, n)
         lst = [self.members[j] for j in L]
         return ox.plot_graph_routes(self.graph, [[self.members[i]], lst])
 
-    def djikstra(self, sommet):
+    def djikstra(self, sommet: int) -> list:
+        if sommet in self.dists_from:
+            return self.dists_from[sommet]
+        """
+        algorithme de recherche des chemins les plus courts
+        """
         distances = [(inf, i, None) for i in range(self.size)]
         distances[sommet] = (0, sommet, sommet)
         heap = []
@@ -94,106 +126,99 @@ class Graph:
                 heappush(heap, (dist_actuelle + self.adj[sommet_actuel,voisin], voisin, sommet_actuel))
             non_vus[sommet_actuel] = False
 
+        self.dists_from[sommet] = distances
         return distances
 
-    def voisins_a_traiter(self, sommet, non_vus):
+    def voisins_a_traiter(self, sommet: int, non_vus: list) -> list:
         v = []
         for i in range(self.size):
             if self.adj[sommet,i] != 0 and non_vus[i]:
                 v.append(i)
         return v
 
-    def chemin(self, i, j):
-        dists = self.djikstra(i)
-        if dists[j][0] == inf:
+    def chemin(self, i: int, j: int) -> (float, list):
+        """
+        renvoie le chemin le plus court de i a j
+        et la distance de ce parcours
+        """
+        if not i in self.dists_from:
+            self.djikstra(i)
+        if self.dists_from[i][j][0] == inf:
             return (inf, [])
         else:
             actuel = j
             c = [j]
             while actuel != i:
-                c.append(dists[actuel][2])
-                actuel = dists[actuel][2]
-                print(actuel)
-            return (dists[j], c)
+                c.append(self.dists_from[i][actuel][2])
+                actuel = self.dists_from[i][actuel][2]
+            return (self.dists_from[i][j][0], c)
 
-def graph_from_ox(G) -> Graph:
-    members = [i for i in G.nodes]
-    n = G.order()
-    adj = np.zeros((n, n))
+    def etendue_minimale_moyenne(self) -> float:
+        """
+        Renovoie la distance minimle entre deux points moyenne
+        """
+        M = Stats([])
+        for i in range(self.size):
+            dists = []
+            for j in self.links(i, 1):
+                dists.append(self.adj[i, j])
+            if dists != []:
+                M.append(min(dists))
+        return M.avg()
 
-    orig_adj = [i for i in G.adjacency()]
-    for i in range(n):
-        successors = [j for j in G.successors(members[i])]
-        for j in range(n):
-            if members[j] in successors:
-                adj[i, j] = orig_adj[i][1][members[j]][0]["length"]
+class Stats:
+    """
+    classe d'utilitaires pour listes, pour effectuer des statistiques
+    """
+    def __init__(self, L: list):
+        self.data = L
+        self.size = len(L)
+        self.sorted = sorted(L)
 
-    return Graph(members, adj, ox.plot_graph(G))
+    def sum(self):
+        S = 0
+        for i in self.data:
+            S += i
+        return S
 
-
-def deg(i: int, G: Graph) -> int:
-    d = 0
-    for j in G.members:
-        if G.adj[i, j] != 0:
-            d += 1
-    return d
-
-
-# donne le degré infini (évalué a n niveaux de parenté) de chaque point
-def deginf(G: Graph, pt: int, lev: int) -> int:
-    degG = [deg(i, G) for i in G.members]
-    if n == 0:
-        return degG[pt]
-    deginfG = degG[:]  # liste des deginf des pts de G
-    for j in links(G, 0, n):
-        deginfG[j] += deginf(G, j, lev - 1)
+    def avg(self):
+        return self.sum()/self.size
 
 
-# donne l'ensemble des noeuds a n niveaux de parenté
-def links(G: Graph, pt: int, n: int) -> list:
-    if n == 0:
-        return None
-    L = []
-    for i in G.members:
-        if G.adj[pt, i] is not None:
-            L.append(i)
-    for i in L:
-        L += links(G, i, n - 1)
-    return L
+    def q1(self):
+        return self.get_median(self.sorted[:self.size // 2])
 
 
-def etendue_minimale_moyenne(G: list):
-    M = []
-    for i in G.members:
-        dists = []
-        for j in links(G, i, 1):
-            dists.append(G.adj[i, j])
-        M.append(min(dists))
-    return avg(M)
+    def q3(self):
+        return self.get_median(self.sorted[self.size // 2:])
+
+    def median(self):
+        return self.sorted[self.size // 2]
+
+    def append(self, elem):
+        self.data.append(elem)
+        self.size += 1
+
+    def get_median(self, lst):
+        """
+        suppose une lst triee
+        """
+        return lst[len(lst) // 2]
+
+    def stats(self):
+        print("Data:", self.data[:min([10, self.size])])
+        print("Average:", self.avg())
+        print("Q1:", self.q1())
+        print("Median:", self.median())
+        print("Q3:", self.q3())
+
+# Telecharge a neuf et enregistre
+
+# G = Graph(place="Saint Germain en Laye, France", filepath="st_ger.gml")
+# G = Graph(address="1 Venelle Artemis, Saint Germain en Laye, France", filepath="st_ger.gml")
+
+# Utilise le graphe preenregistre
+
+G = Graph(filepath="st_ger.gml")
 
 
-def avg(L):
-    S = 0
-    for i in L:
-        S += i
-    return S / len(L)
-
-
-def Q1(lst):
-    sort = sorted(lst)
-    med, i = median(sort)
-    return median(sort[:i])
-
-
-def Q3(lst):
-    sort = sorted(lst)
-    med, i = median(sort)
-    return median(sort[i:])
-
-
-# supposes une liste triée
-def median(lst):
-    return lst[len(lst) // 2]
-
-
-K = Graph(H)
